76e4ca2bc0972b8d15ecf9bc804d4d7b166e2ac7
baldm0mma <jev.forsberg@gmail.com>
2019-09-15 12:56:22 -0600

    Add Header.vue component

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Nasa's Daily Astronomy Photos for {{currentMonthName}}, {{currentYear}}</h1>
+  </div>
+</template>
+
+<script>
+import { currentMonthName, currentYear } from "../../utilz/dates";
+
+export default {
+  name: "Header",
+  data() {
+    return {
+      currentMonthName,
+      currentYear
+    };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+  background-color: #222;
+  color: #fff;
+}
+</style>
