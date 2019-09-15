83e2d2cced7d2ffa7869c0496e267f66e75184f2
baldm0mma <jev.forsberg@gmail.com>
2019-09-15 12:58:59 -0600

    Add DailyThumbnail.vue logic and styling

---
+++ src/components/DailyThumbnail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{daily.title}}</h3>
+    <img :src="daily.url" :alt="daily.title"/>
+    <p>{{daily.date}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['daily']
+  }
+</script>
+
+<style scoped>
+img {
+  box-shadow: 0 0 35px black;
+  width: 80%;
+}
+</style>
