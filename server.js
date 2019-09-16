19992d6193fd58138911d6ee9e8cf4d60cc85fc9
baldm0mma <jev.forsberg@gmail.com>
2019-09-16 17:43:35 -0600

    Server attempt number 2

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+// const express = require('express');
+// const serveStatic = require('serve-static');
+// const path = require('path');
+// app = express();
+// app.use(serveStatic(path.join(__dirname, 'dist')));
+// const port = process.env.PORT || 8080;
+// app.listen(port);
+
+
 const express = require('express');
-const serveStatic = require('serve-static');
 const path = require('path');
+const serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8080;
+app.use(serveStatic(__dirname + "/dist"));
+const port = process.env.PORT || 5000;
 app.listen(port);
+console.log('server started '+ port);
