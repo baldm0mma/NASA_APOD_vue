2234691188a4df9b3612e47c1d1a00d3a9c70e55
baldm0mma <jev.forsberg@gmail.com>
2019-09-16 16:02:48 -0600

    Hide API KEY

--- utilz/urlz.js
+++ utilz/urlz.js
@@ -1,6 +1,5 @@
 import { firstDayOfMonth, currentDayOfMonth } from './dates';
 import { API_KEY } from '../apiKey';
-console.log('api', API_KEY)
 
 export const fullMonthUrl = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${firstDayOfMonth}&end_date=${currentDayOfMonth}`;
 
