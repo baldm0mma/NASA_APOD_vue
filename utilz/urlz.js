5fd4ef1bb46f55e3b8991050e33eeba8c56b5bf4
baldm0mma <jev.forsberg@gmail.com>
2019-09-16 18:08:19 -0600

    Remove apiKey file, and replace with env variable

--- utilz/urlz.js
+++ utilz/urlz.js
@@ -1,5 +1,4 @@
 import { firstDayOfMonth, currentDayOfMonth } from './dates';
-// import { API_KEY } from '../apiKey';
 const API_KEY = process.env.VUE_APP_API_KEY;
 
 export const fullMonthUrl = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${firstDayOfMonth}&end_date=${currentDayOfMonth}`;
