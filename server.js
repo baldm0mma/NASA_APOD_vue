bdb507753ccb9b27fabbb68c69fc7990fd452635
baldm0mma <jev.forsberg@gmail.com>
2019-09-16 18:02:54 -0600

    Remove server

--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 // const serveStatic = require('serve-static');
 // const path = require('path');
 // app = express();
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-// const port = process.env.PORT || 8080;
+// app.use(serveStatic(path.join('public/index.html')));
+// const port = process.env.PORT || 5000;
 // app.listen(port);
 
 
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-const port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+// const express = require('express');
+// const path = require('path');
+// const serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic("public/index.html" + "/dist"));
+// const port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
