1909d8930e0b9482030bd8e4c397dee74492e189
baldm0mma <jev.forsberg@gmail.com>
2019-09-18 10:07:44 -0600

    Delete node_mods, reinstall, and save @vue/cli-service package globally

--- utilz/urlz.js
+++ utilz/urlz.js
@@ -1,6 +1,5 @@
 import { firstDayOfMonth, currentDayOfMonth } from './dates';
 const API_KEY = process.env.VUE_APP_API_KEY;
-console.log(API_KEY);
 
 export const fullMonthUrl = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${firstDayOfMonth}&end_date=${currentDayOfMonth}`;
 
